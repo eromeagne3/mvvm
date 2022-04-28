c637d0c6fa6381a6d14ccad7ea9062e0fa08d2c6
柴田 佳直 <shibata.yoshinao@sofix.co.jp>
2022-04-28 23:24:14 +0900

    Add: プロパティ変更通知インターフェース追加

---
+++ src/Em3.CommonUtilities.MVVM/Em3.CommonUtilities.MVVM/IPropertyChangedNotificattor.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Em3.CommonUtilities.MVVM
+{
+    /// <summary>
+    /// プロパティ変更通知イベントの発行インターフェース
+    /// </summary>
+    public interface IPropertyChangedNotificattor : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// プロパティに値をセットし、PropertyChangedイベントを発生させる。
+        /// </summary>
+        /// <param name="field">値設定先</param>
+        /// <param name="newValue">新しい値</param>
+        /// <param name="propertyName">プロパティ名</param>
+        /// <returns>変更されればtrue</returns>
+        bool Set<T>(ref T field, T newValue, [CallerMemberName]string propertyName = "");
+    }
+}
